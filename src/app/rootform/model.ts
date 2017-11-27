349e3377958ac0ecaedd6223970b966be1f94abb
Ilija Bajunovic <ilija.bajunovic@student.unisg.ch>
2017-11-27 11:51:54 +0100

    Task 5a

--- src/app/rootform/model.ts
+++ src/app/rootform/model.ts
@@ -4,4 +4,15 @@ export class chatMessage {
  ) { }
  }
 
+// export class Person {
+//	private static _nickname: string;
 
+//	public static get Nickname(): string {
+//	return this._nickname;
+//	}
+
+//	public static set Nickname(value: string): {
+//	this._nickname = value;
+//	}
+
+// }
--- src/app/rootform/rootform.component.ts
+++ src/app/rootform/rootform.component.ts
@@ -13,37 +13,50 @@ export class RootformComponent implements OnInit {
 	chatMessage = new chatMessage('');
 	messageHistory: string = '';
 	nickname: string = '';
+	lastnickname: string = '';
 
 
   constructor() { }
 
   ngOnInit() {
-  }
+}
+
 
 public addMessage(message: string): void {
 
-if (this.nickname == '') {
+if (!this.nickname) {
 alert('Gib einen Nickname ein!');
 	
 } else {
 
-	if (this.chatMessage.message == '') {
+	if (!this.chatMessage.message) {
 	alert('Gib eine Nachricht ein!');
 	
 	} else {
 
-	this.messageHistory += this.nickname + ': ' + message + '\n'; 		// Nachricht zur History hinzufuegen
-	this.chatMessage.message = ''; 						// Eingabefeld resetten
+		if (this.nickname == this.lastnickname) {
+
+		this.messageHistory += message + '\n';											// Falls wieder derselbe Benutzer schreibt, brauchen wir keinen Timestamp und nickname
+		this.chatMessage.message = '';
+
+		} else {
+
+			this.messageHistory += 'Am ' + new Date().toLocaleDateString() + ' um ' + new Date().toLocaleTimeString() + ' schrieb ' + this.nickname + ': ' + message + '\n'; 		// Nachricht zur History hinzufuegen
+			this.chatMessage.message = ''; 											// Eingabefeld resetten
+			this.lastnickname = this.nickname; 										// Damit wir wissen, wer zuletzt geschrieben hat
 
 
+			}
+		}
+	}
 }
-}
-}
+
 
 public setNickname(nick: string): void {
 
 	this.nickname = nick;
-}
+	
+	}
 
 
 }
