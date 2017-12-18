12602e2f45bfe5836efbbdb99c7424e41d491e36
Ilija Bajunovic <ilija.bajunovic@student.unisg.ch>
2017-12-18 16:15:46 +0100

    Finish

--- server.js
+++ server.js
@@ -3,29 +3,43 @@ var path = require('path');
 var express = require('express');
 var app = express();
 var history = '';
+var noclients = 0;
+var noclientsmsg = '';
 
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', function(req, res, next) {
-  res.sendFile("index.html");
+  res.sendFile("/home/rootsocket/server/index.html");
 });
 
-app.get('/delete', function(req, res, next) { 			// Funktion eingebaut, um die History zu loeschen indem man 127.0.0.1:8090/delete aufruft
+app.get('/delete', function(req, res, next) {
     history = '';
-  res.sendFile("delete.html");
+  res.sendFile('/home/rootsocket/server/delete.html');
 });
+
 
 var server = http.createServer(app);
 var io = require('socket.io')(server);
-
-
 io.on('connection', function (socket) {
-	socket.emit('msg', { msg: history }); 			// Sobald jemand connectet, bekommt er die history zugeschickt
 
-	socket.on('msg',function(msg){ 				// Nachrichten lauschen
-		socket.broadcast.emit('msg', { msg: msg }); 	// Nachrichten die empfangen wurden allen anderen zuschicken
-		history += msg;					// Zur History hinzufuegen fuer spaetere Benutzer
+	socket.emit('msg', { msg: history });
+
+
+        noclients = noclients + 1;
+	socket.broadcast.emit('no', { no: noclients });
+	socket.emit('no', { no: noclients });
+
+	socket.on('msg',function(msg){
+    		socket.broadcast.emit('msg', { msg: msg });
+		history += msg;
     	})
+
+    	socket.on('disconnect', function(msg){
+		noclients = noclients - 1;
+		socket.broadcast.emit('no', { no: noclients });
+    	})
+	
 });
 
-server.listen(8090); 						// Port setzen und initiieren
+
+server.listen(8080);
